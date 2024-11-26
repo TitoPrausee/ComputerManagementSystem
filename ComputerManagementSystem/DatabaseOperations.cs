2d9df5a5e7bd92625a43f87950a50060b6ecb793
Tito <145434937+TitoPrausee@users.noreply.github.com>
2024-11-26 09:17:04 +0100

    Es Funktionierte skusi

--- ComputerManagementSystem/DatabaseOperations.cs
+++ ComputerManagementSystem/DatabaseOperations.cs
@@ -1,11 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 
 public class DatabaseOperations
 {
-    private string connectionString = "Server=localhost;Database=computermanagementsystem;User  ID=Tiberius;Password=123456;";
+    // Korrigierte Verbindungszeichenfolge
+    private string connectionString = "Server=localhost;Database=computermanagementsystem;Uid=Tiberius;Pwd=123456;";
 
     public List<ComputerSystem> GetComputerSystems()
     {
@@ -14,28 +14,41 @@ public class DatabaseOperations
 
         using (MySqlConnection connection = new MySqlConnection(connectionString))
         {
-            connection.Open();
-            using (MySqlCommand command = new MySqlCommand(query, connection))
+            try
             {
-                using (MySqlDataReader reader = command.ExecuteReader())
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
-                    while (reader.Read())
+                    using (MySqlDataReader reader = command.ExecuteReader())
                     {
-                        ComputerSystem system = new ComputerSystem
+                        while (reader.Read())
                         {
-                            Id = reader.GetInt32(0),
-                            Name = reader.GetString(1),
-                            IPAddress = reader.GetString(2),
-                            OperatingSystem = reader.GetString(3),
-                            LastUpdated = reader.GetDateTime(4),
-                            Motherboard = reader.GetString(5),
-                            GPU = reader.GetString(6),
-                            RAM = reader.GetInt32(7),
-                            Storage = reader.GetInt32(8)
-                        };
-                        systems.Add(system);
+                            ComputerSystem system = new ComputerSystem
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                IPAddress = reader.GetString(2),
+                                OperatingSystem = reader.GetString(3),
+                                LastUpdated = reader.GetDateTime(4),
+                                Motherboard = reader.GetString(5),
+                                GPU = reader.GetString(6),
+                                RAM = reader.GetInt32(7),
+                                Storage = reader.GetInt32(8)
+                            };
+                            systems.Add(system);
+                        }
                     }
                 }
+            }
+            catch (MySqlException ex)
+            {
+                // Fehlerbehandlung für MySQL-spezifische Fehler
+                Console.WriteLine("MySQL Fehler: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Allgemeine Fehlerbehandlung
+                Console.WriteLine("Allgemeiner Fehler: " + ex.Message);
             }
         }
 
